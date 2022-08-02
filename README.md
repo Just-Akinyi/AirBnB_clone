856061b4d1ff1ee5088a5526e92abde5f9709bdf
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-08-02 14:23:57 +0100

    3. BaseModel

--- README.md
+++ README.md
@@ -2,18 +2,17 @@
    <img src="https://camo.githubusercontent.com/8f0a6c42cd692f81f959778174d8192b702ef18c07fcf532208b9fcf73a77774/68747470733a2f2f646176696b616e692e73332e65752d776573742d332e616d617a6f6e6177732e636f6d2f696d616765732f686f6c626572746f6e2f686f6c626572746f6e2e706e67">
 </p>
 
-## Table of contents.</br>
+# Table of contents.</br>
 * Introduction
 * Installation
 * Usage
 * AUTHORS
 
-## Introduction
+# Introduction
 
 In this project, we are developing full clone of AirBnB wep app.
 
 ## Installation
-
 
 Clone the repository with the following command
 
@@ -24,7 +23,7 @@ After cloning the repository Navigate to the "AirBnB_clone" repository
     cd AirBnB_clone
     
 
-## Usage
+## How to use our project
 
 ### Interactive mode
 
--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
 """Defines all common attributes/methods for other classes"""
+from pydoc import classname
 from uuid import uuid4
 from datetime import datetime
 
 
-class Basemodel:
+class BaseModel:
     """Base class"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initializing Base class
         Args:
             *args:
             **kwargs (dict):
         """
         self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.update_at = datetime.today()
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
 
     def save(self):
         """updates the attribute updated_at with the current datetime"""
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        a key __class__ must be added to this dictionary with the class name
+        of the object
+        """
+        dictionnary = self.__dict__
+        dictionnary['__class__'] = self.__class__.__name__
+        dictionnary['created_at'] = self.created_at.isoformat()
+        dictionnary['update_at'] = self.update_at.isoformat()
+        return dictionnary
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
