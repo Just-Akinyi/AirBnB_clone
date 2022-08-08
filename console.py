9319cd3f79d953aba0ca0b1f9f3087bf66a3e09b
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2022-08-08 16:53:29 +0100

    7. Console 0.1 doing...

--- console.py
+++ console.py
@@ -4,10 +4,21 @@
 import cmd
 from signal import signal, SIGINT
 from sys import exit
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    classes = {'BaseModel': BaseModel, 'User': User, 'City': City,
+               'Place': Place, 'Amenity': Amenity, 'Review': Review,
+               'State': State}
 
     def do_quit(self, *args):
         """Quit command to exit the program."""
@@ -25,6 +36,32 @@ class HBNBCommand(cmd.Cmd):
         """Handle the SIGINT or CTRL-C signal"""
         print("^C")
         exit(0)
+
+    def do_create(self, arg):
+        if not arg:
+            print("** class name missing **")
+        elif arg in HBNBCommand.classes.keys():
+            instance = HBNBCommand.classes[arg]()
+            instance.save()
+            print(instance.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, args):
+        args = args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif len(args) == 1 and args not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif (len(args) == 1 and args in HBNBCommand.classes.keys()):
+            print("** instance id missing **")
+        else:
+            instances = models.storage.all()
+            keys = args[0] + '.' + args[1]
+            if keys in instances:
+                print(instances[keys])
+            else:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
